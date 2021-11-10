8b72f83923af46ce77265e85093894da8d610db7
sedrubal <dev@sedrubal.de>
2021-11-10 16:45:15 +0100

    Add missing result_json_types.py

---
+++ result_json_types.py
@@ -0,0 +1,66 @@
+"""Python types for result.json files."""
+
+from typing import Optional, TypedDict, Union
+
+
+class JSONTestDescr(TypedDict):
+    """A test description as parsed from result.json."""
+
+    name: str
+    desc: str
+
+
+class JSONMeasurementDescr(TypedDict):
+    """A measurement description as parsed from result.json."""
+
+    name: str
+    desc: str
+    theoretical_max_value: Optional[float]
+    repetitions: Optional[int]
+
+
+RawTestResultResult = Optional[str]
+
+
+class JSONTestResult(TypedDict):
+    """A test result as parsed from result.json."""
+
+    abbr: str
+    result: RawTestResultResult
+
+
+class JSONMeasurement(TypedDict):
+    """A measurement result as parsed from result.json."""
+
+    abbr: str
+    result: RawTestResultResult
+    details: str
+
+
+class JSONImageMetadata(TypedDict):
+    """Metadata about an image."""
+
+    image: str
+    id: str
+    repo_digests: Optional[list[str]]
+    versions: list[str]
+    created: Optional[str]
+    compliant: Optional[bool]
+
+
+class JSONResult(TypedDict):
+    """The unmodified content of result.json."""
+
+    id: Optional[str]
+    start_time: float
+    end_time: float
+    log_dir: str
+    servers: list[str]
+    clients: list[str]
+    urls: dict[str, str]
+    images: Optional[dict[str, JSONImageMetadata]]
+    tests: dict[str, Union[JSONTestDescr, JSONMeasurementDescr]]
+    quic_draft: int
+    quic_version: str
+    results: list[list[JSONTestResult]]
+    measurements: list[list[JSONMeasurement]]
