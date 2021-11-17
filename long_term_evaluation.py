441342197f67cb0f550844ae990e053d0f9dda3a
sedrubal <dev@sedrubal.de>
2021-11-17 19:21:25 +0100

    Update long term evaluation, support for measurements with plots

--- long_term_evaluation.py
+++ long_term_evaluation.py
@@ -2,18 +2,64 @@
 
 import argparse
 import enum
-import sys
+from collections import defaultdict
 from functools import cached_property
 from itertools import product
 from pathlib import Path
-from typing import Optional
+from typing import Iterable, Optional
 
+from matplotlib import pyplot as plt
 from termcolor import colored, cprint
 
 from result_parser import Result
-from utils import YaspinWrapper, existing_dir_path
+from units import DataRate
+from utils import Subplot, YaspinWrapper, existing_dir_path, natural_data_rate
 
-Series = list[Optional[bool]]
+Series = list[tuple[Optional[bool], Optional[float]]]
+
+# limit values to that value. Every other value seems to be buggy.
+MAX_AVG = 100 * DataRate.MBPS
+
+
+COLORS = [
+    "xkcd:darkish red",
+    "xkcd:terracotta",
+    "xkcd:true green",
+    "xkcd:light indigo",
+    "xkcd:moss",
+    "xkcd:toxic green",
+    "xkcd:cobalt blue",
+    "xkcd:dark lime",
+    "xkcd:cornflower blue",
+    "xkcd:bright aqua",
+    "xkcd:hot purple",
+    "xkcd:tealish green",
+    "xkcd:light lilac",
+    "xkcd:indian red",
+    "xkcd:pinky red",
+    "xkcd:darkish green",
+    "xkcd:fern",
+    "xkcd:lemon",
+    "xkcd:bright cyan",
+    "xkcd:tealish",
+    "xkcd:earth",
+    "xkcd:medium grey",
+    "xkcd:ugly pink",
+    "xkcd:parchment",
+    "xkcd:grey purple",
+    "xkcd:turtle green",
+    "xkcd:sickly green",
+    "xkcd:amethyst",
+    "xkcd:light bluish green",
+    "xkcd:steel",
+    "xkcd:piss yellow",
+    "xkcd:pale gold",
+    "xkcd:lipstick",
+    "xkcd:midnight",
+    "xkcd:chocolate brown",
+    "xkcd:lemon lime",
+    "xkcd:grey brown",
+]
 
 
 def parse_args():
@@ -73,9 +119,9 @@ class AnalyzeResult(enum.IntEnum):
 
     @classmethod
     def from_series(cls, series: Series) -> "AnalyzeResult":
-        num_unknown = sum(1 for result in series if result is None)
-        num_succeeded = sum(1 for result in series if result is True)
-        num_failed = sum(1 for result in series if result is False)
+        num_unknown = sum(1 for (succeeded, _avg) in series if succeeded is None)
+        num_succeeded = sum(1 for (succeeded, _avg) in series if succeeded is True)
+        num_failed = sum(1 for (succeeded, _avg) in series if succeeded is False)
 
         if num_unknown / len(series) > cls._unknown_threshold:
             return cls.OTHER
@@ -141,6 +187,9 @@ class AnalyzeResult(enum.IntEnum):
         return cls.OTHER
 
 
+AnalyzeResults = dict[str, dict[AnalyzeResult, list[tuple[str, Series]]]]
+
+
 class Cli:
     def __init__(
         self, logs_dir: Path, combinations: list[str], testcases: list[str], debug=False
@@ -175,7 +224,11 @@ class Cli:
 
                     continue
 
-                results.append(Result(result_path))
+                #  spinner.write(f"Loading {result_path}...")
+                result = Result(result_path)
+                result.load_from_json()
+                results.append(result)
+                spinner.text = f"Loading result files ({len(results)})"
 
         results.sort(key=lambda result: result.end_time)
 
@@ -183,8 +236,16 @@ class Cli:
 
     @cached_property
     def all_combinations(self) -> set[str]:
-        clients = {client.name for result in self.results for client in result.clients}
-        servers = {server.name for result in self.results for server in result.servers}
+        clients = {
+            client_name
+            for result in self.results
+            for client_name in result.clients.keys()
+        }
+        servers = {
+            server_name
+            for result in self.results
+            for server_name in result.servers.keys()
+        }
 
         return {f"{server}_{client}" for server, client in product(servers, clients)}
 
@@ -192,7 +253,7 @@ class Cli:
         results = self.results
         combinations_to_inspect = self.combinations or self.all_combinations
         max_combination_len = max([len(combi) for combi in combinations_to_inspect])
-        analyze_results = dict[str, dict[AnalyzeResult, list[tuple[str, Series]]]]()
+        analyze_results = AnalyzeResults()
 
         with YaspinWrapper(text="Analyzing", debug=self.debug, color="cyan") as spinner:
             for testcase in self.testcases:
@@ -213,13 +274,35 @@ class Cli:
 
                     for test_result in results:
                         try:
-                            succeeded = test_result.get_test_result(
-                                server=server, client=client, test_abbr=testcase
-                            ).succeeded
-                        except KeyError:
-                            succeeded = None
+                            result = test_result.get_measurement_result(
+                                server=server,
+                                client=client,
+                                measurement_abbr=testcase,
+                            )
+                            succeeded = result.succeeded
 
-                        test_results.append(succeeded)
+                            if succeeded:
+                                unit = DataRate.from_str(result.unit)
+                                avg = result.avg * unit
+
+                                if avg > MAX_AVG:
+                                    spinner.write(
+                                        f"Got avg value {avg} in {test_result.file_path} {combination}, which seems very unlikely. Ignoring."
+                                    )
+                                    avg = None
+                            else:
+                                avg = None
+                        except KeyError:
+                            try:
+                                succeeded = test_result.get_test_result(
+                                    server=server, client=client, test_abbr=testcase
+                                ).succeeded
+                                avg = None
+                            except KeyError:
+                                succeeded = None
+                                avg = None
+
+                        test_results.append((succeeded, avg))
 
                     analyze_result = AnalyzeResult.from_series(test_results)
                     analyze_results[testcase][analyze_result].append(
@@ -251,7 +334,7 @@ class Cli:
                         else colored("✔", color="white", on_color="on_green")
                         if succeeded
                         else colored("⨯", color="white", on_color="on_red")
-                        for succeeded in series
+                        for (succeeded, _avg) in series
                     )
                     print(
                         f"{combination:{max_combination_len}}",
@@ -260,6 +343,78 @@ class Cli:
 
                 if not combinations:
                     cprint("*No combinations*", color="grey", attrs=["italic"])
+
+        self.plot_avgs(analyze_results)
+
+    def plot_avgs(self, analyze_results: AnalyzeResults):
+        dates = [result.end_time for result in self.results]
+        with Subplot(nrows=len(analyze_results)) as (fig, axs):
+
+            axs: Iterable[plt.Axes] = [axs] if len(analyze_results) == 1 else axs
+            fig.title = "Long Term Evaluation of QUIC Interop Runner results"
+
+            for ax, (testcase, combinations_by_analzyse_results) in zip(
+                axs, analyze_results.items()
+            ):
+                ax.set_title(f"Measurement Case: {testcase}")
+                ax.set_xlabel("Run")
+                ax.set_ylabel("Average Goodput of each Implementation")
+                ax.yaxis.set_major_formatter(lambda val, _pos: natural_data_rate(val))
+                ax.grid()
+
+                avgs_by_server = defaultdict[str, list[list[Optional[float]]]](
+                    list[list[Optional[float]]]
+                )
+
+                for category in (
+                    AnalyzeResult.ALWAYS_SUCCESS,
+                    AnalyzeResult.ALMOST_ALWAYS_SUCCESS,
+                    AnalyzeResult.GOT_FIXED,
+                ):
+                    for combination, series in combinations_by_analzyse_results[
+                        category
+                    ]:
+                        server_name, _client_name = combination.split("_")
+                        values: list[Optional[float]] = [
+                            (avg if avg else None) for (_success, avg) in series
+                        ]
+                        avgs_by_server[server_name].append(values)
+
+                for server_index, (server, avg_series) in enumerate(
+                    sorted(avgs_by_server.items())
+                ):
+                    avgs_for_server = list[Optional[float]]()
+
+                    for run_index in range(len(avg_series[0])):
+                        avg_values: list[float] = [
+                            series[run_index]
+                            for series in avg_series
+                            if series[run_index] is not None
+                        ]
+
+                        if avg_values:
+                            avgs_for_server.append(sum(avg_values) / len(avg_values))
+                        else:
+                            avgs_for_server.append(None)
+
+                    ax.plot(
+                        dates,
+                        avgs_for_server,
+                        marker=".",
+                        label=server,
+                        color=COLORS[server_index % len(COLORS)],
+                    )
+
+                ax.legend(title="Server Implementation")
+
+            testcases_str = "-".join(sorted(analyze_results.keys()))
+            fig.savefig(
+                f"long_term_evaluation-{testcases_str}.png",
+                dpi=300,
+                #  transparent=True,
+                bbox_inches="tight",
+            )
+            plt.show()
 
 
 def main():
--- units.py
+++ units.py
@@ -13,6 +13,16 @@ class DataRate:
     MBPS: ClassVar[int] = 10 ** 6
     GBPS: ClassVar[int] = 10 ** 9
 
+    @classmethod
+    def from_str(cls, value: str) -> int:
+        """Return DataRate unit from string."""
+        mapping = {
+            "KBPS": cls.KBPS,
+            "MBPS": cls.MBPS,
+            "GBPS": cls.GBPS,
+        }
+        return mapping[value.upper()]
+
 
 class Time:
     S: ClassVar[int] = 1
