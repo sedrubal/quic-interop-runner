57c5a46ebcd98c84fa6c2ec53f25d2e5dc63fbd0
sedrubal <dev@sedrubal.de>
2021-11-23 12:03:53 +0100

    Update plot_stats.sh

--- plot_stats.sh
+++ plot_stats.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -eu
 
-RESULT=https://interop.sedrubal.de/logs/logs_sat_2021-11-07/result.json
+RESULT_SIM=https://interop.sedrubal.de/logs/logs_sat_2021-11-07/result.json
+RESULT_REAL=https://interop.sedrubal.de/logs/logs_real_2021-11-15/result.json
 MEASUREMENTS=(G SAT SATL)
 IMG_PATH=../../img/plots/
 
@@ -11,13 +12,15 @@ function run {
 }
 
 for extra_arg in "" "--efficiency"; do
-    run ./plot_stats.py --no-interactive "${extra_arg}" "--img-path=${IMG_PATH}" --plot-type=boxplot ${MEASUREMENTS[@]} "${RESULT}"
-    run ./plot_stats.py --no-interactive "${extra_arg}" "--img-path=${IMG_PATH}" --plot-type=kdes ${MEASUREMENTS[@]} "${RESULT}"
+    run ./plot_stats.py --no-interactive "${extra_arg}" "--img-path=${IMG_PATH}" --plot-type=boxplot ${MEASUREMENTS[@]} "${RESULT_SIM}"
+    run ./plot_stats.py --no-interactive "${extra_arg}" "--img-path=${IMG_PATH}" --plot-type=kdes ${MEASUREMENTS[@]} "${RESULT_SIM}"
 
     for measurement in "${MEASUREMENTS[@]}"; do
-        run ./plot_stats.py --no-interactive "${extra_arg}" "--img-path=${IMG_PATH}" --plot-type=heatmap "${measurement}" "${RESULT}"
-        run ./plot_stats.py --no-interactive "${extra_arg}" "--img-path=${IMG_PATH}" --plot-type=ridgeline "${measurement}" "${RESULT}"
+        run ./plot_stats.py --no-interactive "${extra_arg}" "--img-path=${IMG_PATH}" --plot-type=heatmap "${measurement}" "${RESULT_SIM}"
+        run ./plot_stats.py --no-interactive "${extra_arg}" "--img-path=${IMG_PATH}" --plot-type=ridgeline "${measurement}" "${RESULT_SIM}"
     done
 done
+
+run ./plot_pairplot.py --no-interactive "--img-path=${IMG_PATH}" "${RESULT_SIM}" "${RESULT_REAL}"
 
 echo "Don't forget to plot using ./plot_real_experiments.py!"
