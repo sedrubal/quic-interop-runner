a35bc50422f6ca6daa9ea452fbbe2f73a0c5b80c
sedrubal <dev@sedrubal.de>
2021-11-29 09:18:19 +0100

    Enhance collect plots script

--- collect_plots.py
+++ collect_plots.py
@@ -3,8 +3,8 @@
 import shutil
 import sys
 from argparse import ArgumentParser
-from pathlib import Path
 from enum import Enum
+from pathlib import Path
 
 from termcolor import colored, cprint
 
@@ -19,13 +19,21 @@ class CollectMode(Enum):
 
 
 def parse_args():
-    """docstring for parse_args"""
     parser = ArgumentParser()
     parser.add_argument(
         "--clean", action="store_true", help="Clean collect dir before."
     )
-    parser.add_argument("--force", action="store_true", help="Update existing files.")
-    parser.add_argument("result", type=Result, help="The result file to use.")
+    parser.add_argument(
+        "--force",
+        action="store_true",
+        help="Update existing files.",
+    )
+    parser.add_argument(
+        "results",
+        nargs="+",
+        type=Result,
+        help="The result files to use.",
+    )
     parser.add_argument(
         "--collect-mode",
         action="store",
@@ -44,14 +52,27 @@ def parse_args():
         help="The mode of plotting to collect",
     )
     parser.add_argument(
-        "collect_dir", type=Path, help="The directory to collect the images."
+        "-o",
+        "--collect-dir",
+        type=Path,
+        help="The directory to collect the images.",
+    )
+    parser.add_argument(
+        "-t",
+        "--test",
+        "-m",
+        "--measurement",
+        dest="meas_abbr",
+        nargs="*",
+        help="The measurements to use.",
     )
 
     return parser.parse_args()
 
 
 def collect_plots(
-    result: Result,
+    results: list[Result],
+    meas_abbrs: list[str],
     collect_dir: Path,
     collect_mode: CollectMode,
     plot_modes: list[PlotMode],
@@ -63,40 +84,51 @@ def collect_plots(
 
     collect_dir.mkdir(parents=True, exist_ok=True)
 
-    for meas in result.get_all_measurements_of_type("SAT", succeeding=True):
-        files = [
-            meas.log_dir_for_test.path / f"time_{mode.value}_plot.png"
-            for mode in plot_modes
-        ]
+    for result in results:
+        for meas_abbr in meas_abbrs:
+            for meas in result.get_all_measurements_of_type(meas_abbr, succeeding=True):
+                files = [
+                    meas.log_dir_for_test.path / f"time_{mode.value}_plot.png"
+                    for mode in plot_modes
+                ]
 
-        for png in files:
-            if not png.is_file():
-                cprint(f"Plot {png} does not exist.", color="red")
+                for png in files:
+                    if not png.is_file():
+                        cprint(f"Plot {png} does not exist.", color="red")
 
-                continue
+                        continue
 
-            target = collect_dir / f"{meas.combination}_{png.name}"
+                    target = collect_dir / f"{meas.combination}_{png.name}"
 
-            if target.is_file():
-                if force:
-                    target.unlink()
-                else:
-                    sys.exit(
-                        colored(f"{target} already exists. Use --force.", color="red")
-                    )
+                    if target.is_file():
+                        if force:
+                            target.unlink()
+                        else:
+                            sys.exit(
+                                colored(
+                                    f"{target} already exists. Use --force.",
+                                    color="red",
+                                )
+                            )
 
-            if collect_mode == CollectMode.SYMLINK:
-                cprint(f"Symlinking {target} -> {create_relpath(png)}", color="green")
-                target.symlink_to(png)
-            elif collect_mode == CollectMode.COPY:
-                cprint(f"Copying {create_relpath(png)} -> {target}", color="green")
-                shutil.copy(png, target)
+                    if collect_mode == CollectMode.SYMLINK:
+                        cprint(
+                            f"Symlinking {target} -> {create_relpath(png)}",
+                            color="green",
+                        )
+                        target.symlink_to(png)
+                    elif collect_mode == CollectMode.COPY:
+                        cprint(
+                            f"Copying {create_relpath(png)} -> {target}", color="green"
+                        )
+                        shutil.copy(png, target)
 
 
 def main():
     args = parse_args()
     collect_plots(
-        result=args.result,
+        results=args.results,
+        meas_abbrs=args.measurements,
         collect_dir=args.collect_dir,
         collect_mode=args.collect_mode,
         plot_modes=args.plot_mode,
