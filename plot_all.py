f6523fa806edcd1bec2eadfde957a9069f1fcf82
sedrubal <dev@sedrubal.de>
2021-11-30 11:15:30 +0100

    Refactor plot_all to use LOGGER and ProcessPoolExecutor

--- plot_all.py
+++ plot_all.py
@@ -3,17 +3,17 @@
 import argparse
 import sys
 from collections import defaultdict
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
 from typing import Optional
 
-from termcolor import colored, cprint
+from termcolor import colored
 
 from enums import CacheMode, PlotMode, Side
 from plot_diagram import DEFAULT_TITLES, PlotCli
 from result_parser import MeasurementResultInfo, Result
 from trace_analyzer2 import ParsingError
-from utils import TraceTriple, create_relpath, existing_file_path
+from utils import LOGGER, TraceTriple, create_relpath, existing_file_path
 
 DEFAULT_TITLE = "{mode_default_title} ({test_abbr}, server: {server}, client: {client})"
 
@@ -90,6 +90,170 @@ def parse_args():
     return parser.parse_args()
 
 
+def plot_in_meas_run_dir(
+    measurement_result: MeasurementResultInfo,
+    modes: list[PlotMode],
+    current_log_dir: Path,
+    include_failed: bool,
+    annotate: bool,
+    interruptable_logging: bool,
+    output_format: str,
+    title: str,
+    force: bool,
+) -> tuple[str, str, list[str]]:
+    """Generate plot for for this test case."""
+    test_case_dir = measurement_result.log_dir_for_test.path
+
+    ret = (
+        measurement_result.combination,
+        measurement_result.test.abbr,
+        list[str](),
+    )
+
+    if not measurement_result.succeeded and not include_failed:
+        LOGGER.info(
+            "✔ Measurement %s Failed. Skipping. Use --include-failed to include it anyway.",
+            measurement_result.log_dir_for_test.path.relative_to(current_log_dir),
+        )
+        ret[2].append("testacse failed")
+        return ret
+
+    trace_triples = list[TraceTriple]()
+
+    for repetition_dir in measurement_result.repetition_log_dirs:
+        base_sim_path = repetition_dir / "sim"
+        trace_triple = TraceTriple(
+            left_pcap_path=(
+                base_sim_path / f"trace_node_{Side.LEFT.value}_with_secrets.pcapng"
+            ).resolve(),
+            right_pcap_path=(
+                base_sim_path / f"trace_node_{Side.RIGHT.value}_with_secrets.pcapng"
+            ).resolve(),
+        )
+
+        if not trace_triple.left_pcap_path.is_file():
+            LOGGER.error(
+                f"⨯ %s does not exist",
+                trace_triple.left_pcap_path,
+            )
+
+            continue
+
+        if not trace_triple.right_pcap_path.is_file():
+            LOGGER.error(
+                f"⨯ %s does not exist",
+                trace_triple.right_pcap_path,
+            )
+
+            continue
+
+        trace_triples.append(trace_triple)
+
+    if not trace_triples:
+        LOGGER.error(
+            f"⨯ no pcapng files found for %s. Skipping...",
+            test_case_dir,
+        )
+
+        ret[2].append("no trace files found")
+        return ret
+
+    cli = PlotCli(
+        trace_triples=trace_triples,
+        annotate=annotate,
+        cache=CacheMode.LOAD,
+        # enable debug mode with interruptable terminal logging when we use more than 1 worker
+        # debug=debug or self.max_workers > 1,
+        debug=interruptable_logging,
+    )
+
+    for mode in modes:
+        output_file = test_case_dir / f"time_{mode.value}_plot.{output_format}"
+        err_output_file = test_case_dir / f".time_{mode.value}_plot_error.txt"
+
+        if not force:
+            if output_file.is_file():
+                LOGGER.debug(
+                    "✔ %s already exists. Skipping. Use --force to overwrite.",
+                    output_file.relative_to(current_log_dir),
+                )
+
+                ret[2].append("already exists")
+
+                continue
+            elif err_output_file.is_file():
+                with err_output_file.open("r") as file:
+                    err_msg = file.read().strip()
+
+                try:
+                    broken_pcap, err_msg = err_msg.splitlines()
+                except ValueError:
+                    LOGGER.error(
+                        "Error message %s has invalid format.",
+                        err_output_file,
+                    )
+                    broken_pcap = None
+
+                LOGGER.error(
+                    (
+                        "⨯ Trace could not be plotted in previous run. "
+                        f"{err_output_file.relative_to(current_log_dir)} exists: "
+                    ),
+                )
+                if broken_pcap:
+                    LOGGER.error(broken_pcap)
+                LOGGER.error(err_msg)
+                LOGGER.error(
+                    "Skipping. Use --force to overwrite.",
+                )
+
+                ret[2].append(err_msg)
+
+                continue
+
+        LOGGER.info(
+            "⚒ Plotting in %s (%d trace pairs) → %s",
+            create_relpath(test_case_dir),
+            len(trace_triples),
+            create_relpath(output_file),
+        )
+        cli.set_params(
+            title=title.format(
+                combination=measurement_result.combination,
+                client=measurement_result.client.name,
+                server=measurement_result.server.name,
+                mode_default_title=DEFAULT_TITLES[mode],
+                test_abbr=measurement_result.test.abbr,
+                test_name=measurement_result.test.name,
+                test_desc=measurement_result.test.desc,
+            ),
+            mode=mode,
+            output_file=output_file,
+        )
+        try:
+            cli.run()
+        except ParsingError as err:
+            LOGGER.error(
+                "⨯ Could not parse %s in %s. Skipping...",
+                err.trace,
+                test_case_dir,
+            )
+            LOGGER.error(f"⨯ %s", err)
+
+            with err_output_file.open("w") as file:
+                print(
+                    f"{err.trace.input_file}:\n",
+                    err.msg,
+                    file=file,
+                )
+
+            ret[2].append(err.msg)
+
+            continue
+
+    return ret
+
+
 class PlotAllCli:
     def __init__(
         self,
@@ -97,7 +261,7 @@ class PlotAllCli:
         result_files: list[Path],
         title: str,
         annotate: bool,
-        format: str,
+        output_format: str,
         force=False,
         only_sat=False,
         include_failed=False,
@@ -110,201 +274,26 @@ class PlotAllCli:
         self.only_sat = only_sat
         self.title = title
         self.annotate = annotate
-        self.format = format
+        self.output_format = output_format
         self.force = force
         self.include_failed = include_failed
         self.modes = modes
-        self._current_log_dir: Optional[Path] = None
         self.debug = debug
         self.max_workers = max_workers
         if max_workers > 1 and debug:
             sys.exit("Debug and max_workers > 1 is not advised")
 
-    def plot_in_meas_run_dir(
-        self,
-        measurement_result: MeasurementResultInfo,
-        modes: list[PlotMode],
-    ) -> list[str]:
-        """Generate plot for for this test case."""
-        assert self._current_log_dir
-        test_case_dir = measurement_result.log_dir_for_test.path
-
-        if not measurement_result.succeeded and not self.include_failed:
-            cprint(
-                (
-                    "✔ Measurement "
-                    f"{measurement_result.log_dir_for_test.path.relative_to(self._current_log_dir)} "
-                    "Failed. Skipping. Use --include-failed to include it anyway."
-                ),
-                file=sys.stderr,
-                color="cyan",
-            )
-
-            return ["testcase failed"]
-
-        trace_triples = list[TraceTriple]()
-
-        for repetition_dir in measurement_result.repetition_log_dirs:
-            base_sim_path = repetition_dir / "sim"
-            trace_triple = TraceTriple(
-                left_pcap_path=(
-                    base_sim_path / f"trace_node_{Side.LEFT.value}_with_secrets.pcapng"
-                ).resolve(),
-                right_pcap_path=(
-                    base_sim_path / f"trace_node_{Side.RIGHT.value}_with_secrets.pcapng"
-                ).resolve(),
-            )
-
-            if not trace_triple.left_pcap_path.is_file():
-                cprint(
-                    f"⨯ {trace_triple.left_pcap_path} does not exist",
-                    color="red",
-                    file=sys.stderr,
-                )
-
-                continue
-
-            if not trace_triple.right_pcap_path.is_file():
-                cprint(
-                    f"⨯ {trace_triple.right_pcap_path} does not exist",
-                    color="red",
-                    file=sys.stderr,
-                )
-
-                continue
-
-            trace_triples.append(trace_triple)
-
-        if not trace_triples:
-            cprint(
-                f"⨯ no pcapng files found for {test_case_dir}. Skipping...",
-                file=sys.stderr,
-                color="red",
-            )
-
-            return ["no traces files found"]
-
-        cli = PlotCli(
-            trace_triples=trace_triples,
-            annotate=self.annotate,
-            cache=CacheMode.LOAD,
-            # enable debug mode with interruptable terminal logging when we use more than 1 worker
-            debug=self.debug or self.max_workers > 1,
-        )
-
-        rets = list[str]()
-
-        for mode in modes:
-            output_file = test_case_dir / f"time_{mode.value}_plot.{self.format}"
-            err_output_file = test_case_dir / f".time_{mode.value}_plot_error.txt"
-
-            if not self.force:
-                if output_file.is_file():
-                    cprint(
-                        (
-                            f"✔ {output_file.relative_to(self._current_log_dir)} already exists. "
-                            "Skipping. Use --force to overwrite."
-                        ),
-                        file=sys.stderr,
-                        color="cyan",
-                    )
-
-                    rets.append("already exists")
-
-                    continue
-                elif err_output_file.is_file():
-                    with err_output_file.open("r") as file:
-                        err_msg = file.read().strip()
-
-                    try:
-                        broken_pcap, err_msg = err_msg.splitlines()
-                    except ValueError:
-                        cprint(
-                            f"Error message {err_output_file} has invalid format.",
-                            file=sys.stderr,
-                        )
-                        broken_pcap = None
-
-                    cprint(
-                        (
-                            "⨯ Trace could not be plotted in previous run. "
-                            f"{err_output_file.relative_to(self._current_log_dir)} exists: "
-                        ),
-                        file=sys.stderr,
-                        color="red",
-                    )
-                    if broken_pcap:
-                        cprint(broken_pcap, file=sys.stderr, color="red")
-                    cprint(err_msg, file=sys.stderr, color="red")
-                    cprint(
-                        "Skipping. Use --force to overwrite.",
-                        file=sys.stderr,
-                        color="red",
-                    )
-
-                    rets.append(err_msg)
-
-                    continue
-
-            cprint(
-                (
-                    f"⚒ Plotting in {create_relpath(test_case_dir)} "
-                    f"({len(trace_triples)} trace pairs) → {create_relpath(output_file)}"
-                ),
-                attrs=["bold"],
-            )
-            cli.set_params(
-                title=self.title.format(
-                    combination=measurement_result.combination,
-                    client=measurement_result.client.name,
-                    server=measurement_result.server.name,
-                    mode_default_title=DEFAULT_TITLES[mode],
-                    test_abbr=measurement_result.test.abbr,
-                    test_name=measurement_result.test.name,
-                    test_desc=measurement_result.test.desc,
-                ),
-                mode=mode,
-                output_file=output_file,
-            )
-            try:
-                cli.run()
-            except ParsingError as err:
-                cprint(
-                    (
-                        f"⨯ Could not parse {err.trace} in "
-                        f"{test_case_dir}. "
-                        "Skipping..."
-                    ),
-                    file=sys.stderr,
-                    color="red",
-                )
-                cprint(f"⨯ {err}", file=sys.stderr, color="red")
-
-                with err_output_file.open("w") as file:
-                    print(
-                        f"{err.trace.input_file}:\n",
-                        err.msg,
-                        file=file,
-                    )
-
-                rets.append(err.msg)
-
-                continue
-
-        return rets
-
     def plot_in_log_dir(self, result: Result):
         """Generate plots for result file."""
-        cprint(
-            f"⚒ Plotting results {result} (log dir: {result.log_dir})",
-            attrs=["bold"],
+        LOGGER.info(
+            "⚒ Plotting results %s (log dir: %s)",
+            result,
+            result.log_dir,
         )
-
-        self._current_log_dir = result.log_dir.path
 
         plot_results = defaultdict[str, set[str]](set[str])
 
-        with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
+        with ProcessPoolExecutor(max_workers=self.max_workers) as executor:
             futures = []
             for tmp1 in result.measurement_results.values():
                 for tmp2 in tmp1.values():
@@ -315,21 +304,18 @@ class PlotAllCli:
                     else:
                         measurement_results = list(tmp2.values())
 
-                    def job(measurement_result, modes):
-                        plot_results = self.plot_in_meas_run_dir(
-                            measurement_result, modes
-                        )
-                        return (
-                            measurement_result.combination,
-                            measurement_result.test.abbr,
-                            plot_results,
-                        )
-
                     for measurement_result in measurement_results:
                         future = executor.submit(
-                            job,
+                            plot_in_meas_run_dir,
                             measurement_result,
                             self.modes,
+                            result.log_dir.path,
+                            self.include_failed,
+                            self.annotate,
+                            self.debug or self.max_workers > 1,
+                            self.output_format,
+                            self.title,
+                            self.force,
                         )
                         futures.append(future)
 
@@ -374,7 +360,7 @@ def main():
         only_sat=args.only_sat,
         title=args.title,
         annotate=not args.no_annotation,
-        format=args.format,
+        output_format=args.format,
         force=args.force,
         include_failed=args.include_failed,
         modes=args.mode,
@@ -386,7 +372,7 @@ def main():
     except KeyboardInterrupt:
         sys.exit("\nQuit")
 
-    cprint("✔ Done", color="green", attrs=["bold"])
+    LOGGER.info("✔ Done")
 
 
 if __name__ == "__main__":
